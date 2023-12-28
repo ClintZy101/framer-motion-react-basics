8fc2511a601166a79baea226783d9f5fecab881c
ClintZy101 <clielizek.joy@gmail.com>
2023-12-28 23:24:37 +0900

    view-based animations added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import AnimationControls from "./components/AnimationControls";
+import BasicsOfMotion from "./components/BasicsOfMotion";
+import Gestures from "./components/Gestures";
+import ViewBasedAnimations from "./components/ViewBasedAnimations";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+     {/* <BasicsOfMotion /> */}
+     {/* <Gestures /> */}
+     {/* <AnimationControls /> */}
+     <ViewBasedAnimations />
     </div>
   );
 }
